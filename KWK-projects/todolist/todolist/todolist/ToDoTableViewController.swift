6ed9afba08580406d398a80d1fa6d2023597d7ae
Jamie De Los Santos <77027304+itisjamied@users.noreply.github.com>
2021-08-02 10:27:55 -0400

    To List initial commit

---
+++ KWK-projects/todolist/todolist/todolist/ToDoTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoTableViewController
+//
+//  Created by CTechMBP20E on 8/2/21.
+//
+
+import UIKit
+
+class ToDoTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
